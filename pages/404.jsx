ae50e95e71b442b925654b6a7f28a8c0d2896342
gesora96 <nahidahmed9933@gmail.com>
2022-05-10 23:21:22 +0600

    redirect added

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,10 +1,21 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 5000);
+  }, [router]);
+
   return (
     <div className='not-found'>
       <h1>Ooops...</h1>
       <h2>That page cannot be found</h2>
+      <h5>After 5 sec we will redirect you in the home page</h5>
       <p>
         Go back to the{' '}
         <Link href='/'>
